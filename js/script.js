3d7023d5e655d0ecb590ca7886d3254800c05923
lucadisanto7 <lucadisanto47@gmail.com>
2024-07-18 16:49:00 +0200

    [UPD] Added milestone 3

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// app-milestone2.js
+// app-milestone3.js
 const { createApp, reactive } = Vue;
 
 const App = {
@@ -8,16 +8,30 @@ const App = {
                 { id: 1, text: 'Fare la spesa', done: false },
                 { id: 2, text: 'Pagare le bollette', done: true },
                 { id: 3, text: 'Fare il bucato', done: false }
-            ]
+            ],
+            newTodoText: ''
         });
 
         const removeTodo = (index) => {
             state.todos.splice(index, 1);
         };
 
+        const addTodo = () => {
+            if (state.newTodoText.trim() !== '') {
+                const newTodo = {
+                    id: state.todos.length + 1,
+                    text: state.newTodoText,
+                    done: false
+                };
+                state.todos.push(newTodo);
+                state.newTodoText = '';
+            }
+        };
+
         return {
             state,
-            removeTodo
+            removeTodo,
+            addTodo
         };
     }
 };
